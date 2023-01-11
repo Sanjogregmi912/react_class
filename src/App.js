98744edf23c998c948ca5e353209b38c4e465b0c
Sanjog Regmi <mailsanjog.regmi@gmail.com>
2023-01-11 08:27:04 +0545

    forms extraction

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-// import { useState } from 'react';
+
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-// import Feedback_app  from './components/Feedback_app';
-import History_app from './components/History_app';
 
 
-function App({name,age}) {
+function App() {
+  const [newNote,setNewNote] = useState(' ')
+  const [notes,setnotes] = useState([])
 
-  
-  // const [counter,setCounter]= useState(0)
+  useEffect(()=>{
+    axios.get("http://localhost:3001/notes").then((response)=>{
+      console.log(response)
+      setnotes(response.data)
+    })
+},[])
 
-  // const handleCounter = () =>{
-  //   setCounter(counter+1)
-  // }
+  const handleInputChange = (event)=>{
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const HandleAdd = (event) =>{
+    event.preventDefault()
+    alert("Testing... ")
+  }
 
 
   return (
     <>
    
-   {/* <h2>{counter}</h2>
-   <button onClick={handleCounter}>Plus</button> */}
-   {/* <Feedback_app/> */}
-   <History_app/>
+      <h2>Notes</h2>
+      <ul>
+        {notes.map(note => <li key = {note.id}><p>{note.content}</p> <p>{note.date}</p></li>)}
+        
+      </ul>
+      <form>
+        <input value = {newNote} onChange = {handleInputChange}/> 
+        <button onClick={HandleAdd}> add </button>
+      </form>
 
     </>
     
