55443aedca7dc0a4395ccc305795a503c1f43bcb
Sanjog Regmi <mailsanjog.regmi@gmail.com>
2023-01-11 09:54:16 +0545

    forms completed

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import './App.css';
 
 
 
-function App() {
+function App(props) {
   const [newNote,setNewNote] = useState(' ')
-  const [notes,setnotes] = useState([])
+  const [notes,setnotes] = useState(props.notes)
+ const [showAll ,setshowall] =  useState(true)
+
+
+ const notestoshow =  showAll ? notes
+ : notes.filter(n => n.important === true)
 
   useEffect(()=>{
     axios.get("http://localhost:3001/notes").then((response)=>{
@@ -23,8 +28,39 @@ function App() {
 
   const HandleAdd = (event) =>{
     event.preventDefault()
-    alert("Testing... ")
+    // create notes
+
+    const note = {
+      id : notes.lenght +1,
+      content : newNote,
+      date : new Date().toString(),
+      important : Math.random() < 0.5
+
+
+
+    }
+
+
+    if(newNote !== "")setnotes(notes.concat(note))
+    setNewNote("")
+
   }
+
+  const setdelete =  (id) =>{
+    var response = window.confirm(`do you really want to delete note with id ${id}`)
+    if(response === true){
+      setnotes(notes.filter(n => n.id !== id))
+    }
+    else{
+      setnotes(notes)
+      
+    }
+    
+
+  }
+const tooglebutton = showAll ? "show impotant" : "show all"
+
+
 
 
   return (
@@ -32,14 +68,15 @@ function App() {
    
       <h2>Notes</h2>
       <ul>
-        {notes.map(note => <li key = {note.id}><p>{note.content}</p> <p>{note.date}</p></li>)}
+        {notestoshow.map(note => <li key = {note.id}><p>{note.content} <button onClick={()=>setdelete(note.id)}>delete</button></p> <p>{note.date}</p></li>)}
         
       </ul>
+      <button onClick={()=> setshowall(!showAll)}>{tooglebutton} </button>
       <form>
         <input value = {newNote} onChange = {handleInputChange}/> 
         <button onClick={HandleAdd}> add </button>
       </form>
-
+      
     </>
     
 
